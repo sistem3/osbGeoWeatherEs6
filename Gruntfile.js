2b047797ff5c23dd1ec79b33c0bf4173c14f2a19
Ben Burton <sistematic3@yahoo.co.uk>
2016-01-08 15:11:16 +0000

    Moving CSS to Sass (adding compile task)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,24 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/osb-geo-weather.css': 'scss/osb-geo-weather.scss'
+                }
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-serve');
+    grunt.loadNpmTasks('grunt-sass');
 
     // Default task(s).
     grunt.registerTask('default', ['serve']);
+    grunt.registerTask('css', ['sass']);
 
 };
